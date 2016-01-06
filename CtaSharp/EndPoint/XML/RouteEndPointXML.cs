80a209329d4932e3ee4c95bcf028c58176919b88
SergueiFedorov <blackraven36@gmail.com>
2016-01-05 21:33:16 -0600

    Removed unused references

--- CtaSharp/EndPoint/XML/RouteEndPointXML.cs
+++ CtaSharp/EndPoint/XML/RouteEndPointXML.cs
@@ -1,14 +1,8 @@
-﻿using CtaSharp.EndPoint;
-using CtaSharp.Models;
+﻿using CtaSharp.Models;
 using CtaSharp.Parameters;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
-using CtaSharp.Tools.XML;
 using CtaSharp.EndPoint.XML.Converters;
 using CtaSharp.EndPoint.XML;
 
--- CtaSharp/Parameters/TrainParameters.cs
+++ CtaSharp/Parameters/TrainParameters.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CtaSharp
+﻿namespace CtaSharp
 {
     public class TrainParameters
     {
