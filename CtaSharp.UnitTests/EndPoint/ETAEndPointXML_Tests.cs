bedbfd39f359067b81bb8079d415edad7524d491
Serguei Fedorov <sergueifedorov@Sergueis-MacBook-Pro.local>
2016-01-08 23:16:40 -0600

    Revert "Migrating to NUnit for the unit test framework"
    This reverts commit 4e3aeb195a725e34a0dc05e0e5afebcf1eb2d30d.

---
+++ CtaSharp.UnitTests/EndPoint/ETAEndPointXML_Tests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CtaSharp.UnitTests.EndPoint
+{
+    [TestClass]
+    class ETAEndPointXML_Tests
+    {
+        [TestMethod]
+        public void Basic()
+        {
+
+        }
+    }
+}
