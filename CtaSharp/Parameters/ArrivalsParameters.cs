eef8891da80295c2a733d26e094503a5e298988d
Serguei Fedorov <sergueifedorov@Sergueis-MacBook-Pro.local>
2016-01-10 15:35:41 -0600

    Added exceptions to ArrivalsParameters as per CTA spec

--- CtaSharp/Parameters/ArrivalsParameters.cs
+++ CtaSharp/Parameters/ArrivalsParameters.cs
@@ -4,8 +4,36 @@ namespace CtaSharp
 {
 	public class ArrivalsParameters
 	{
-		public int? NumericStationIdentifier { get; set; }
-		public int? NumericStopIdentifier { get; set; }
+		private int? _NumericStationIdentifier { get; set; }
+		public int? NumericStationIdentifier 
+		{ 
+			get {
+				return _NumericStationIdentifier;
+			}
+			set {
+				if (this.NumericStopIdentifier.HasValue) {
+					throw new Exception ("Numeric stop identifyer has already been set");
+				}
+
+				this._NumericStationIdentifier = value;
+			}
+		}
+
+		private int? _NumericStopIdentifier { get; set; }
+		public int? NumericStopIdentifier 
+		{ 
+			get {
+				return _NumericStopIdentifier;
+			}
+			set {
+				if (this.NumericStationIdentifier.HasValue) {
+					throw new Exception ("Numeric station identifyer has already been set");
+				}
+
+				this._NumericStopIdentifier = value;
+			}
+		}
+
 		public int? MaximumResults { get; set; }
 		public string RouteCode { get; set; }
 	}
