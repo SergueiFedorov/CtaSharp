bad59f66fe981403ad90e8dd8fa60ad2d004d7a6
Serguei Fedorov <sergueifedorov@Sergueis-MacBook-Pro.local>
2016-01-12 21:07:08 -0600

    Extension refresh method for Route

---
+++ CtaSharp/Extension/RouteExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using CtaSharp.Models;
+using CtaSharp.Parameters;
+using CtaSharp.EndPoint;
+
+namespace CtaSharp
+{
+	public static class RouteExtensionMethods
+	{
+		public static void Refresh(this Route route)
+		{
+			var result = route.EndPoint.Get (new RouteParameters () { Route = route.TrainRoute }).SingleOrDefault();
+
+			if (result == null)
+			{
+				throw new Exception ("Unable to refresh route. No results returned endpoint.");
+			}
+
+			route.EndPoint = result.EndPoint;
+			route.TrainRoute = result.TrainRoute;
+			route.Trains = result.Trains;
+
+		}
+	}
+}
+
