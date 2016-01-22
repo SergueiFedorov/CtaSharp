8e8469ef76f83c857d1835dba5e247bf60165fd5
SergueiFedorov <blackraven36@gmail.com>
2016-01-21 21:28:27 -0600

    Vehicle converter test basic layout

---
+++ CtaSharp.UnitTests/XMLToVehicleConverter._Tests.cs
@@ -0,0 +1,21 @@
+﻿using CtaSharp.Shared.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtaSharp.UnitTests
+{
+    class XMLToVehicleConverter
+    {
+        [Test]
+        public void BasicConversion()
+        {
+
+        }
+
+
+    }
+}
