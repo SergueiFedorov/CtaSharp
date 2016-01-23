e948a11eb760f41200af53c484fd92deea37c9f8
Serguei Fedorov <sergueifedorov@Sergueis-MacBook-Pro.local>
2016-01-22 23:31:51 -0600

    Updated model and converter

--- CtaSharp.BusTracker/EndPoint/Converters/XMLToVehicleConverter.cs
+++ CtaSharp.BusTracker/EndPoint/Converters/XMLToVehicleConverter.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml;
+using CtaSharp.Shared;
+using CtaSharp.Tools.XML;
 
 namespace CtaSharp.BusTracker.EndPoint.Converters
 {
@@ -12,7 +16,25 @@ namespace CtaSharp.BusTracker.EndPoint.Converters
     {
         public IEnumerable<Vehicle> Convert(string XML, string parentNodeName)
         {
-            throw new NotImplementedException();
+			if (string.IsNullOrEmpty (XML) || string.IsNullOrEmpty (parentNodeName)) 
+			{
+				throw new ArgumentNullException ();
+			}
+
+			var parsedXML = XDocument.Parse(XML);
+			var parentNode = parsedXML.Descendants().Where(x => x.Name == parentNodeName);
+			var vehicles = parentNode.SelectMany(x => x.Descendants().Where(y => y.Name == "vehicle"));
+
+			foreach (var vehicleXML in vehicles) {
+				XMLParsingTools.ExtractValue (vehicleXML, "vid");
+				XMLParsingTools.ExtractValue (vehicleXML, "tmstmp");
+				XMLParsingTools.ExtractValue (vehicleXML, "");
+				XMLParsingTools.ExtractValue (vehicleXML, "");
+				XMLParsingTools.ExtractValue (vehicleXML, "");
+				XMLParsingTools.ExtractValue (vehicleXML, "");
+			}
+
+			throw new ArgumentNullException ();
         }
     }
 }
--- CtaSharp.BusTracker/Models/Vehicle.cs
+++ CtaSharp.BusTracker/Models/Vehicle.cs
@@ -8,5 +8,15 @@ namespace CtaSharp.BusTracker.Models
 {
     class Vehicle
     {
+		public int VehicleId { get; set; }
+		public decimal Latitude { get; set; }
+		public decimal Longitude { get; set; }
+		public int HeadingDegrees { get; set; }
+		public int Route { get; set; }
+		public string Destination { get; set; }
+		public int Speed { get; set; }
+		public int ParentPatternID { get; set; }
+		public int ParentDistance { get; set; }
+		public bool Delayed { get; set; }
     }
 }
