f0c19678e4007d24e0c2c1a8c62169f1b2d67216
SergueiFedorov <blackraven36@gmail.com>
2016-01-25 20:16:52 -0600

    Converter and datasource for bus routes

---
+++ CtaSharp.BusTracker/EndPoint/Converters/XMLToRouteConverter.cs
@@ -0,0 +1,18 @@
+﻿using CtaSharp.BusTracker.Models;
+using CtaSharp.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtaSharp.BusTracker.EndPoint.Converters
+{
+    class XMLToRouteConverter : IConverter<Route>
+    {
+        public IEnumerable<Route> Convert(string XML, string parentNodeName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CtaSharp.BusTracker/EndPoint/DataSource/RoutesDataSource.cs
@@ -0,0 +1,21 @@
+﻿using CtaSharp.Shared;
+using CtaSharp.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtaSharp.BusTracker.EndPoint.DataSource
+{
+    class RoutesDataSource : DataSourceBase, IDataSource
+    {
+        const string EndpointAddress = "http://www.ctabustracker.com/bustime/api/v1/getroutes";
+
+        public RoutesDataSource()
+            : base(EndpointAddress)
+        {
+
+        }
+    }
+}
